aec4bff4fffbb1a664f56e8be2fbeb40de1a0c31
Jvondamm <joshuavd@att.net>
2022-01-31 16:58:19 -0800

    fixed input list formatting and added FIFO algo and print funcs

--- sched_sim.py
+++ sched_sim.py
@@ -1,22 +1,44 @@
 import sys
 
+def print_job(a, b, c):
+    print('Job {0:03d} -- '.format(a), end='')
+    print('Turnaround {:.2f}  Wait {:.2f}'.format(b, c))
+
+def print_avg(a, b):
+    print('Average -- Turnaround {:.2f}  Wait {:.2f}'.format(a, b))
+
+def sched_fifo(jobs):
+    avg_turnaround = 0
+    avg_wait = 0
+    total_time = 0
+    job_count = 0
+    for job in jobs:
+        cur_wait = total_time
+        total_time += job[0]
+        cur_turnaround = total_time
+        avg_wait += cur_wait
+        avg_turnaround += cur_turnaround
+        print_job(job_count, cur_turnaround, cur_wait)
+        job_count += 1
+    print_avg(avg_turnaround / len(jobs), avg_wait / len(jobs))
+
+def sched_srtn(jobs):
+    return
+
+def sched_rr(jobs, q):
+    return
+
+
 def main():
     num_args = len(sys.argv)
     algo = 'FIFO'
     q = 1
-    jobs = []
 
     if num_args > 1:
         file_name = sys.argv[1]
-
         with open(file_name) as job_file:
-            lines = [lines.rstrip('\n') for line in job_file]
-        
-        for line in lines:
-            tokens = line.split()
-            token[0] = int(token[0])
-            token[1] = int(token[1])
-            jobs.append(tokens)
+            # for each line split into int list and sort by second column (arrival time)
+            jobs = sorted(([list(map(int, line.rstrip('\n').split())) for line in job_file]), key=lambda x: x[1])
 
         if num_args == 2:
             sched_fifo(jobs, q)
@@ -47,18 +69,11 @@ def main():
         sys.exit()
 
     if algo == 'SRTN':
-        sched_srtn(jobs, q)
+        sched_srtn(jobs)
     if algo == 'FIFO':
-        sched_fifo(jobs, q)
+        sched_fifo(jobs)
     if algo == 'RR':
         sched_rr(jobs, q)
-
-
-def sched_srtn(jobs, q):
-
-def sched_fifo(jobs, q):
-
-def sched_rr(jobs, q):
 
 
 if __name__ == '__main__':
